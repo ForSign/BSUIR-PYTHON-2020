b85732ee2bedc81328f0c640ad601b5782470adf
ForSign <38140644+ForSign@users.noreply.github.com>
2020-04-17 20:33:04 +0300

    Add files via upload

--- Solutions/Task1/853502_Timothey_Shiryaev/lab.py
+++ Solutions/Task1/853502_Timothey_Shiryaev/lab.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from collections import Counter
 import re
+import argparse
+from argparse import Namespace
 from array import *
 import numpy as np
 
@@ -10,28 +12,42 @@ print(">>>Hello world!")
 dictor = dict()
 print()
 
-with open("TexForLab.txt") as file:
-    lines = file.readline()
-    # print(lines)
-    words = re.sub('[^a-zA-Z ]', '', lines)
-    words = words.split()
+
+def parsArgs():
+    parser = argparse.ArgumentParser(description="Lab1:")
+    parser.add_argument("-f", "--file", default="TexForLab.txt", type=open, help="Opens up a file for text read")
+    parser.add_argument("-t", "--task", default=None, type=int, help="Opens up a specific task for running")
+    parser.add_argument("-c", "--count", default=20, type=int, help="Amount of fibonacci numbers")
+    return parser
+
+
+def fibonacci_generator():
+    a,b = 1,1
+    yield a
+    yield b
+    while True:
+        a, b = b, a+b
+        yield b
 
 # 1
-dictor.update(Counter(words))
-for element in dictor:
-    print(element, " was ", dictor.get(element))
-else:
-    dictor.clear()
-    print()
+def tsk1():
+    dictor.update(Counter(words))
+    for element in dictor:
+        print(element, " was ", dictor.get(element))
+    else:
+        dictor.clear()
+        print()
+
 
 # 2
-dictor.update(Counter(words).most_common(10))
-print("String: ", end="")
-for i in dictor:
-    print(str(i).lower(), end=" ")
-else:
-    print()
-    print()
+def tsk2():
+    dictor.update(Counter(words).most_common(10))
+    print("String: ", end="")
+    for i in dictor:
+        print(str(i).lower(), end=" ")
+    else:
+        print()
+        print()
 
 
 # 3, 4
@@ -49,10 +65,10 @@ def partition(numb, low, high):
     i = low - 1
     pivot = numb[high]  # warn we take right element as a pivot
     for j in range(low, high):
-        if nums[j] < pivot:
+        if numb[j] < pivot:
             i += 1
-            nums[j], nums[i] = nums[i], nums[j]
-    nums[i+1], nums[high] = nums[high], nums[i+1]
+            numb[j], numb[i] = numb[i], numb[j]
+    numb[i+1], numb[high] = numb[high], numb[i+1]
     return i + 1
 
 
@@ -95,15 +111,58 @@ def merge_sort(numb):
         k += 1
 
 
-nums = getnumbers_stringLike()
-print("Quick sort:")
-print("#right element taken as a pivot", end="\n\n")
-print(nums)
-quick_sort(numb=nums, low=0, high=len(nums) - 1)
-print(nums, end='\n\n')
+def tsk3():
+    nums = getnumbers_stringLike()
+    print("Quick sort:")
+    print("#right element taken as a pivot", end="\n\n")
+    print(nums)
+    quick_sort(numb=nums, low=0, high=len(nums) - 1)
+    print(nums, end='\n\n')
 
-nums = getnumbers_stringLike()
-print("Merge sort:")
-print(nums)
-merge_sort(numb=nums)
-print(nums)
+
+def tsk4():
+    nums = getnumbers_stringLike()
+    print("Merge sort:")
+    print(nums)
+    merge_sort(numb=nums)
+    print(nums)
+
+
+def tsk5():
+    seq = fibonacci_generator()
+    print([next(seq) for _ in range(params.count)])
+
+
+def choose(argument):
+    switcher = {
+        1: tsk1,
+        2: tsk2,
+        3: tsk3,
+        4: tsk4,
+        5: tsk5,
+    }
+    switcher.get(argument, lambda: "Invalid month")()
+
+# main
+params = parsArgs().parse_args()
+
+if params.file:
+    lines = params.file.readline()
+    # print(lines)
+    words = re.sub('[^a-zA-Z ]', '', lines)
+    words = words.split()
+else:
+    with open("TexForLab.txt") as file:
+        lines = file.readline()
+        # print(lines)
+        words = re.sub('[^a-zA-Z ]', '', lines)
+        words = words.split()
+
+if params.task:
+    choose(params.task)
+else:
+    choose(1)
+    choose(2)
+    choose(3)
+    choose(4)
+    choose(5)
